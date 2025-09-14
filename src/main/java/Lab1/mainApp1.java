978e81e685c0ee20c915552fd02dfcbb88f6d788
EricLabile <yliangbyhk@gmail.com>
2025-09-14 21:54:02 +0800

    update for the reference code

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -1,8 +1,12 @@
 package Lab1;
-
+/* Comp3111Lex\Lab1\mainApp1.java
+Main application for Lab1 exercise */
 public class mainApp1 {
     public static void main(String[] args) {
-        System.out.println("Lab1 Demo Program");
-        System.out.println("Hello COMP3111");
+        int b = 2;
+        int n = 11;
+        System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
+        System.out.println(n + "! = " + myLibrary.factorial(n));
+        System.out.println("Program ..Ended ...");
     }
 }
--- src/main/java/Lab1/myLibrary.java
+++ src/main/java/Lab1/myLibrary.java
@@ -1,18 +1,20 @@
 package Lab1;
-
+/* Comp3111LEx\Lab1\myLibrary.java
+my Java library consists of 2 mathematical functions */
 public class myLibrary {
-    public static int power(int base, int exp) {
-        int result = 1;
-        for (int i = 0; i < exp; i++) {
-            result *= base;
+    public static int Power(int Base, int Exponent){
+        if (Exponent == 1){
+            return Base * Exponent;
+        } else {
+            return Base * Power(Base, Exponent - 1);
         }
-        return result;
     }
 
-    public static int factorial(int n) {
-        if (n <= 1) return 1;
-        return n * factorial(n - 1);
+    public static int factorial(int n){
+        if (n == 1){
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
     }
-
-    public static int square(int n) { return n * n; }
 }
